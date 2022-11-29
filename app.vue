55de9097f7676e62d72d16c696332e828950e955
Timothee-Picard <timothee@atnos.com>
2022-11-29 15:39:53 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+ #app {
+   min-height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+ }
+</style>
